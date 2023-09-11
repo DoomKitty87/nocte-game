071f956eeaf23f4eac72577cc5dae1a296f695f4
DoomKitty87 <doomkitty887@gmail.com>
2023-09-10 19:28:52 -0700

    made generation into a static class

---
+++ Assets/_Scripts/HydraulicErosion.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static NoiseMaps;
+
+public class HydraulicErosion : MonoBehaviour
+{
+    
+    public int xSize = 32;
+    public int zSize = 32;
+    
+    public int xTiles = 1;
+    public int zTiles = 1;
+
+    public float scale = 1000;
+
+    public float amplitude = 50;
+    
+    public Material material;
+
+    private Mesh[] _meshes;
+
+    private void Start()
+    {
+        scale = 1 / scale;
+        for (int z = 0; z < zTiles; z++)
+        {
+            for (int x = 0; x < xTiles; x++)
+            {
+                GameObject go = new GameObject("Tile");
+                go.transform.parent = transform;
+                
+                MeshFilter mf = go.AddComponent<MeshFilter>();
+                MeshRenderer mr = go.AddComponent<MeshRenderer>();
+                mr.material = material;
+                mf.mesh = new Mesh();
+                Mesh msh = mf.mesh;
+                msh.vertices = NoiseMaps.GenerateTerrain(x * xSize, z * zSize, xSize, scale, amplitude);
+                WindTriangles(msh);
+                UpdateMesh(msh);
+                go.transform.position = new Vector3(x * xSize, 0, z * zSize);
+            }
+        }
+    }
+
+    private void WindTriangles(Mesh targetMesh)
+    {
+        int[] triangles = new int[xSize * zSize * 6];
+        int vert = 0;
+        int tris = 0;
+        for (int z = 0; z < zSize; z++)
+        {
+            for (int x = 0; x < xSize; x++)
+            {
+                triangles[tris] = vert + 0;
+                triangles[tris + 1] = vert + xSize + 1;
+                triangles[tris + 2] = vert + 1;
+                triangles[tris + 3] = vert + 1;
+                triangles[tris + 4] = vert + xSize + 1;
+                triangles[tris + 5] = vert + xSize + 2;
+                vert++;
+                tris += 6;
+            }
+            vert++;
+        }
+
+        targetMesh.triangles = triangles;
+    }
+
+    private static void UpdateMesh(Mesh targetMesh)
+    {
+        targetMesh.RecalculateNormals();
+    }
+
+}
---
+++ Assets/_Scripts/NoiseMaps.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Mathematics;
+using static Unity.Mathematics.noise;
+using Unity.Jobs;
+using Unity.Burst;
+using Unity.Collections;
+
+public static class NoiseMaps
+{
+
+    [BurstCompile]
+    public struct SimplexNoiseJob : IJobParallelFor
+    {
+
+        public int chunkSize;
+        public float xOffset;
+        public float zOffset;
+        public float scale;
+        public float amplitude;
+
+        [WriteOnly] public NativeArray<float> output;
+
+        public void Execute(int index)
+        {
+            float sampleX = index % chunkSize;
+            float sampleZ = index / chunkSize;
+            float noiseValue1 = snoise(new float2((sampleX + xOffset) * scale, (sampleZ + zOffset) * scale));
+            float noise1 = noiseValue1 * (noiseValue1 > 0.4f ? Mathf.SmoothStep(0, 1, (noiseValue1 - 0.4f) / 0.6f) * amplitude * 15 + amplitude : amplitude);
+            float noiseValue2 = snoise(new float2((sampleX + xOffset) * (scale / 2), (sampleZ * zOffset) * (scale / 2)));
+            float noise2 = noiseValue2 * amplitude / 2;
+            float noiseValue3 = snoise(new float2((sampleX + xOffset) * (scale / 4), (sampleZ * zOffset) * (scale / 4)));
+            float noise3 = noiseValue3 * amplitude / 4;
+            float noise = noise1 + noise2 + noise3;
+            output[index] = noise;
+        }
+
+    }
+
+    public static Vector3[] GenerateTerrain(float xOffset, float zOffset, int xSize, float scale, float amplitude)
+    {
+        var jobResult = new NativeArray<float>((xSize + 1) * (xSize + 1), Allocator.TempJob);
+
+        var job = new SimplexNoiseJob()
+        {
+            chunkSize = xSize + 1,
+            xOffset = xOffset,
+            zOffset = zOffset,
+            scale = scale,
+            amplitude = amplitude,
+            output = jobResult
+        };
+
+        var handle = job.Schedule(jobResult.Length, 32);
+        handle.Complete();
+        Vector3[] vertices = new Vector3[(xSize + 1) * (xSize + 1)];
+        for (int z = 0, i = 0; z <= xSize; z++)
+        {
+            for (int x = 0; x <= xSize; x++)
+            {
+                vertices[i] = new Vector3(x, jobResult[i], z);
+                i++;
+            }
+        }
+
+        jobResult.Dispose();
+        return vertices;
+    }
+
+}
