bee6a81b64ab097c28c8ab1af1232bcb13dab2e2
DoomKitty87 <doomkitty887@gmail.com>
2023-09-12 17:06:21 -0700

    finished lod

--- Assets/_Scripts/LODGeneration.cs
+++ Assets/_Scripts/LODGeneration.cs
@@ -18,9 +18,11 @@ public class LODGeneration : MonoBehaviour
     public float _xResolution = 0.25f;
     public float _zResolution = 0.25f;
     
-    public int _xTiles = 100;
-    public int _zTiles = 100;
+    public int _xTiles = 101;
+    public int _zTiles = 101;
     public float _scale = 1000;
+
+    public int _lodNoFalloffDist = 10;
 
     public float _amplitude = 50;
     public int _octaves = 10;
@@ -66,7 +68,11 @@ public class LODGeneration : MonoBehaviour
             }
             
             for (int i = 0; i < _zTiles; i++) {
-                UpdateTile(_tilePositions[0, i], 0, playerXChunkScale - (_xTiles - 1) / 2, i + playerZChunkScale - (_zTiles - 1) / 2);
+                int lod = 0;
+                if (Mathf.Abs(0 - (_xTiles - 1) * 2) > 10 || Mathf.Abs(i - (_zTiles - 1) * 2) > 10) {
+                    lod = 2;
+                }
+                UpdateTile(_tilePositions[0, i], lod, playerXChunkScale - (_xTiles - 1) / 2, i + playerZChunkScale - (_zTiles - 1) / 2);
             }
         }
         else if (deltaX > 0) {
@@ -86,7 +92,11 @@ public class LODGeneration : MonoBehaviour
             }
             
             for (int i = 0; i < _zTiles; i++) {
-                UpdateTile(_tilePositions[_xTiles - 1, i], 0, playerXChunkScale + (_xTiles - 1) / 2, i + playerZChunkScale - (_zTiles - 1) / 2);
+                int lod = 0;
+                if (Mathf.Abs(_xTiles - 1 - (_xTiles - 1) * 2) > 10 || Mathf.Abs(i - (_zTiles - 1) * 2) > 10) {
+                    lod = 2;
+                }
+                UpdateTile(_tilePositions[_xTiles - 1, i], lod, playerXChunkScale + (_xTiles - 1) / 2, i + playerZChunkScale - (_zTiles - 1) / 2);
             }
         }
 
@@ -107,7 +117,11 @@ public class LODGeneration : MonoBehaviour
             }
             
             for (int i = 0; i < _xTiles; i++) {
-                UpdateTile(_tilePositions[i, 0], 0, i + playerXChunkScale - (_xTiles - 1) / 2, playerZChunkScale - (_zTiles - 1) / 2);
+                int lod = 0;
+                if (Mathf.Abs(i - (_xTiles - 1) * 2) > 10 || Mathf.Abs(0 - (_zTiles - 1) * 2) > 10) {
+                    lod = 2;
+                }
+                UpdateTile(_tilePositions[i, 0], lod, i + playerXChunkScale - (_xTiles - 1) / 2, playerZChunkScale - (_zTiles - 1) / 2);
             }
         }
         else if (deltaZ > 0) {
@@ -127,7 +141,11 @@ public class LODGeneration : MonoBehaviour
             }
             
             for (int i = 0; i < _xTiles; i++) {
-                UpdateTile(_tilePositions[i, _zTiles - 1], 0, i + playerXChunkScale - (_xTiles - 1) / 2, playerZChunkScale + (_zTiles - 1) / 2);
+                int lod = 0;
+                if (Mathf.Abs(i - (_xTiles - 1) * 2) > 10 || Mathf.Abs(_zTiles - 1 - (_zTiles - 1) * 2) > 10) {
+                    lod = 2;
+                }
+                UpdateTile(_tilePositions[i, _zTiles - 1], lod, i + playerXChunkScale - (_xTiles - 1) / 2, playerZChunkScale + (_zTiles - 1) / 2);
             }
         }
         
@@ -147,30 +165,33 @@ public class LODGeneration : MonoBehaviour
         _tilePositions = new int[_xTiles, _zTiles];
         for (int x = -(_xTiles - 1) / 2, i = 0; x <= (_xTiles - 1) / 2; x++)
         {
-            for (int z = -(_zTiles - 1) / 2; z <= (_zTiles - 1) / 2; z++)
-            {
-                GenerateTile(x, z, i);
+            for (int z = -(_zTiles - 1) / 2; z <= (_zTiles - 1) / 2; z++) {
+                int lod = 0;
+                if (Mathf.Abs(x - (_xTiles - 1) * 2) > 10 || Mathf.Abs(z - (_zTiles - 1) * 2) > 10) {
+                    lod = 2;
+                }
+                GenerateTile(x, z, i, lod);
                 i++;
             }
         }
     }
 
-    private void GenerateTile(int x, int z, int index)
+    private void GenerateTile(int x, int z, int index, int lod)
     {
         GameObject go = new GameObject("Tile");
         go.transform.parent = transform;
-                
+        int lodFactor = (int) Mathf.Pow(2, lod);
         MeshFilter mf = go.AddComponent<MeshFilter>();
         MeshRenderer mr = go.AddComponent<MeshRenderer>();
         mr.material = _material;
         mf.mesh = new Mesh();
         Mesh msh = mf.mesh;
-        Vector3[] vertexData = NoiseMaps.GenerateTerrain(x * _xSize * _xResolution + _seed, z * _zSize * _zResolution + _seed, _xSize, _zSize, _scale, _amplitude, _octaves, _easeCurve, _xResolution, _zResolution);
+        Vector3[] vertexData = NoiseMaps.GenerateTerrain(x * _xSize * _xResolution + _seed, z * _zSize * _zResolution + _seed, _xSize / lodFactor, _zSize / lodFactor, _scale, _amplitude, _octaves, _easeCurve, _xResolution * lodFactor, _zResolution * lodFactor);
         msh.vertices = vertexData;
-        WindTriangles(msh);
+        WindTriangles(msh, lodFactor);
         UpdateMesh(msh);
-        float[] temperatureMap = NoiseMaps.GenerateTemperatureMap(vertexData, x * _xSize * _xResolution * _seed, z * _zSize * _zResolution * _seed, _xSize, _zSize, _scale / _temperatureScale, _easeCurve, _xResolution, _zResolution);
-        float[] humidityMap = NoiseMaps.GenerateHumidityMap(vertexData, temperatureMap, x * _xSize * _xResolution / _seed, z * _zSize * _zResolution / _seed, _xSize, _zSize, _scale / _humidityScale, _easeCurve, _xResolution, _zResolution);
+        float[] temperatureMap = NoiseMaps.GenerateTemperatureMap(vertexData, x * _xSize * _xResolution * _seed, z * _zSize * _zResolution * _seed, _xSize / lodFactor, _zSize / lodFactor, _scale / _temperatureScale, _easeCurve, _xResolution * lodFactor, _zResolution * lodFactor);
+        float[] humidityMap = NoiseMaps.GenerateHumidityMap(vertexData, temperatureMap, x * _xSize * _xResolution / _seed, z * _zSize * _zResolution / _seed, _xSize / lodFactor, _zSize / lodFactor, _scale / _humidityScale, _easeCurve, _xResolution * lodFactor, _zResolution * lodFactor);
         if (_hasColliders) go.AddComponent<MeshCollider>();
         go.transform.position = new Vector3(x * _xSize * _xResolution, 0, z * _zSize * _zResolution);
         go.isStatic = true;
@@ -189,26 +210,27 @@ public class LODGeneration : MonoBehaviour
         _tilePool[index].mesh.vertices = NoiseMaps.GenerateTerrain(x * _xSize * _xResolution + _seed, z * _zSize * _zResolution + _seed, _xSize / lodFactor, _zSize / lodFactor, _scale, _amplitude, _octaves, _easeCurve, _xResolution * lodFactor, _zResolution * lodFactor);
         _tilePool[index].temperatureMap = NoiseMaps.GenerateTemperatureMap(_tilePool[index].mesh.vertices, x * _xSize * _xResolution * _seed, z * _zSize * _zResolution * _seed, _xSize / lodFactor, _zSize / lodFactor, _scale / _temperatureScale, _easeCurve, _xResolution * lodFactor, _zResolution * lodFactor);
         _tilePool[index].humidityMap = NoiseMaps.GenerateHumidityMap(_tilePool[index].mesh.vertices, _tilePool[index].temperatureMap, x * _xSize * _xResolution / _seed, z * _zSize * _zResolution / _seed, _xSize / lodFactor, _zSize / lodFactor, _scale / _humidityScale, _easeCurve, _xResolution * lodFactor, _zResolution * lodFactor);
-        WindTriangles(_tilePool[index].mesh);
+        WindTriangles(_tilePool[index].mesh, lodFactor);
         UpdateMesh(_tilePool[index].mesh);
         _tilePool[index].obj.transform.position = new Vector3(x * _xSize * _xResolution, 0, z * _zSize * _zResolution);
     }
 
-    private void WindTriangles(Mesh targetMesh)
-    {
-        int[] triangles = new int[_xSize * _zSize * 6];
+    private void WindTriangles(Mesh targetMesh, int lod) {
+        int xSize = _xSize / lod;
+        int zSize = _zSize / lod;
+        int[] triangles = new int[xSize * zSize * 6];
         int vert = 0;
         int tris = 0;
-        for (int z = 0; z < _zSize; z++)
+        for (int z = 0; z < zSize; z++)
         {
-            for (int x = 0; x < _xSize; x++)
+            for (int x = 0; x < xSize; x++)
             {
                 triangles[tris] = vert + 0;
-                triangles[tris + 1] = vert + _xSize + 1;
+                triangles[tris + 1] = vert + xSize + 1;
                 triangles[tris + 2] = vert + 1;
                 triangles[tris + 3] = vert + 1;
-                triangles[tris + 4] = vert + _xSize + 1;
-                triangles[tris + 5] = vert + _xSize + 2;
+                triangles[tris + 4] = vert + xSize + 1;
+                triangles[tris + 5] = vert + xSize + 2;
                 vert++;
                 tris += 6;
             }
