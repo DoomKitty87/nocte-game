098845bed109858f40acab56f32938426738592d
DoomKitty87 <doomkitty887@gmail.com>
2023-09-12 22:14:18 -0700

    improved lag by spreading it out(tm)

--- Assets/_Scripts/LODGeneration.cs
+++ Assets/_Scripts/LODGeneration.cs
@@ -1,7 +1,6 @@
 using System;
 using UnityEngine;
-using UnityEngine.Serialization;
-
+using System.Collections.Generic;
 public class LODGeneration : MonoBehaviour
 {
     private struct WorldTile
@@ -13,6 +12,14 @@ public class LODGeneration : MonoBehaviour
         public float[] humidityMap;
         public int x;
         public int z;
+        public int lod;
+
+    }
+
+    private struct QueueObject
+    {
+
+        public int index;
         public int lod;
 
     }
@@ -42,6 +49,7 @@ public class LODGeneration : MonoBehaviour
     public AnimationCurve _lodCurve;
 
     public int _maxLODDecrease = 0;
+    public int _maxUpdatesPerFrame = 10;
 
     public bool _hasColliders;
 
@@ -50,6 +58,8 @@ public class LODGeneration : MonoBehaviour
 
     private int _lastPlayerChunkX;
     private int _lastPlayerChunkZ;
+    
+    private List<QueueObject> _updateQueue = new List<QueueObject>();
 
     public void UpdatePlayerLoadedChunks(float playerX, float playerZ)
     {
@@ -80,7 +90,7 @@ public class LODGeneration : MonoBehaviour
                 int lod = (int)(_lodCurve.Evaluate(maxDist / (_xTiles - 1) * 2) * _maxLODDecrease);
                 _tilePool[_tilePositions[0, i]].x = playerXChunkScale - (_xTiles - 1) / 2;
                 _tilePool[_tilePositions[0, i]].z = i + playerZChunkScale - (_zTiles - 1) / 2;
-                UpdateTile(_tilePositions[0, i], lod);
+                QueueTileUpdate(_tilePositions[0, i], lod);
             }
         }
         else if (deltaX > 0) {
@@ -104,7 +114,7 @@ public class LODGeneration : MonoBehaviour
                 int lod = (int)(_lodCurve.Evaluate(maxDist / (_xTiles - 1) * 2) * _maxLODDecrease);
                 _tilePool[_tilePositions[_xTiles - 1, i]].x = playerXChunkScale + (_xTiles - 1) / 2;
                 _tilePool[_tilePositions[_xTiles - 1, i]].z = i + playerZChunkScale - (_zTiles - 1) / 2;
-                UpdateTile(_tilePositions[_xTiles - 1, i], lod);
+                QueueTileUpdate(_tilePositions[_xTiles - 1, i], lod);
             }
         }
 
@@ -129,7 +139,7 @@ public class LODGeneration : MonoBehaviour
                 int lod = (int)(_lodCurve.Evaluate(maxDist / (_xTiles - 1) * 2) * _maxLODDecrease);
                 _tilePool[_tilePositions[i, 0]].x = i + playerXChunkScale - (_xTiles - 1) / 2;
                 _tilePool[_tilePositions[i, 0]].z = playerZChunkScale - (_zTiles - 1) / 2;
-                UpdateTile(_tilePositions[i, 0], lod);
+                QueueTileUpdate(_tilePositions[i, 0], lod);
             }
         }
         else if (deltaZ > 0) {
@@ -153,7 +163,7 @@ public class LODGeneration : MonoBehaviour
                 int lod = (int)(_lodCurve.Evaluate(maxDist / (_xTiles - 1) * 2) * _maxLODDecrease);
                 _tilePool[_tilePositions[i, _zTiles - 1]].x = i + playerXChunkScale - (_xTiles - 1) / 2;
                 _tilePool[_tilePositions[i, _zTiles - 1]].z = playerZChunkScale + (_zTiles - 1) / 2;
-                UpdateTile(_tilePositions[i, _zTiles - 1], lod);
+                QueueTileUpdate(_tilePositions[i, _zTiles - 1], lod);
             }
         }
 
@@ -162,7 +172,7 @@ public class LODGeneration : MonoBehaviour
                 for (int z = 0; z < _zTiles; z++) {
                     float maxDist = Mathf.Max(Mathf.Abs(_tilePool[_tilePositions[x, z]].x - playerXChunkScale), Mathf.Abs(_tilePool[_tilePositions[x, z]].z - playerZChunkScale));
                     int lod = (int)(_lodCurve.Evaluate(maxDist / (_xTiles - 1) * 2) * _maxLODDecrease);
-                    if (lod != _tilePool[_tilePositions[x, z]].lod) UpdateTile(_tilePositions[x, z], lod);
+                    if (lod != _tilePool[_tilePositions[x, z]].lod) QueueTileUpdate(_tilePositions[x, z], lod);
                     if (_hasColliders && maxDist < 2) UpdateCollider(_tilePositions[x, z]);
                     else if (_hasColliders) _tilePool[_tilePositions[x, z]].meshCollider.enabled = false;
                 }
@@ -177,6 +187,15 @@ public class LODGeneration : MonoBehaviour
     {
         _scale = 1 / _scale;
         SetupPool();
+    }
+
+    private void Update() {
+        for (int i = 0; i < _maxUpdatesPerFrame; i++) {
+            if (_updateQueue.Count > 0) {
+                UpdateTile(_updateQueue[0].index, _updateQueue[0].lod);
+                _updateQueue.RemoveAt(0);
+            }
+        }
     }
     
     private void SetupPool()
@@ -234,6 +253,13 @@ public class LODGeneration : MonoBehaviour
         tile.lod = lod;
         _tilePool[index] = tile;
         _tilePositions[index / _zTiles, index % _zTiles] = index;
+    }
+
+    private void QueueTileUpdate(int index, int lod) {
+        QueueObject obj = new QueueObject();
+        obj.index = index;
+        obj.lod = lod;
+        _updateQueue.Add(obj);
     }
     
     //Regenerate given tile based on an LOD parameter.
