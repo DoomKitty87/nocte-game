a900db1d9bb8a0693cdcfd67fae56b0e2424a68b
Matthew Dinh <72724975+feenix1@users.noreply.github.com>
2023-09-20 10:03:14 -0700

    combat

---
+++ Assets/_Scripts/_Entities/CombatCore.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Timeline;
+
+[RequireComponent(typeof(HealthInterface))]
+public class CombatCore : MonoBehaviour
+{
+  [SerializeField] private float _raycastLength;
+  [SerializeField] private float _damage;
+  [SerializeField] private float _range;
+  [SerializeField] private float _cooldown;
+  private float _secondsSinceLastAttack;
+  // Start is called before the first frame update
+  private void Start() {
+    _secondsSinceLastAttack = 0;
+  }
+
+  // Update is called once per frame
+  private void Update() {
+    if (Input.GetAxisRaw("Fire1") > 0 && _secondsSinceLastAttack > _cooldown) {
+      Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, _range);
+      if (hit.collider != null) {
+        hit.collider.GetComponent<HealthInterface>().Damage(_damage);
+      }
+      _secondsSinceLastAttack = 0;
+    }
+    _secondsSinceLastAttack += Time.deltaTime;
+  }
+
+  private void OnDrawGizmos() {
+    Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, _range);
+    Gizmos.color = Color.magenta;
+    Vector3 lineEnd;
+    if (hit.point == null) {
+      lineEnd = transform.forward * _range;
+    }
+    else {
+      lineEnd = hit.point;
+    }
+    Gizmos.DrawLine(transform.position, lineEnd + transform.position);
+  }
+}
