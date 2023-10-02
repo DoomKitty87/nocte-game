4ec772e4b1efe7707eec72dd4ba4688823a83462
SpikyLlama <62197004+SpikyLlama@users.noreply.github.com>
2023-10-02 09:59:52 -0700

    movement ai started ;)

--- Assets/_Scripts/_Entities/Enemies/TestAI.cs
+++ Assets/_Scripts/_Entities/Enemies/TestAI.cs
@@ -2,17 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Movement))]
+
 public class TestAI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Movement _movement;
+    private GameObject _player;
+
+    private void OnValidate()
     {
-        
+        _movement = gameObject.GetComponent<Movement>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        _player = GameObject.FindWithTag("Player");
+    }
+
+    private void Update()
+    {
+        CheckDistance();
+    }
+
+    // deals with encounters (plcl, plnr, plfr, sacl, sanr) 
+    private void CheckDistance()
+    {
+        float distancePlayer = Vector3.Distance(this.transform.position, _player.transform.position);
+        if (distancePlayer < 5)
+        {
+            _movement.SetBoolInputs(true, false);
+        } else
+        {
+            _movement.SetBoolInputs(false, false);
+            _movement.SetInputVector(_player.transform.position - this.transform.position);
+        }
     }
 }
