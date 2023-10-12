557ae88eaa0e8cfec82b104a196eecc379fe074a
Matthew Dinh <72724975+feenix1@users.noreply.github.com>
2023-10-12 10:02:35 -0700

    rotation still stutters and i dont know why

--- Assets/_Scripts/_Entities/PlayerControl.cs
+++ Assets/_Scripts/_Entities/PlayerControl.cs
@@ -9,25 +9,21 @@ public class PlayerControl : MonoBehaviour
 
   [Header("References")] 
   [SerializeField] private Transform _transformAlignToMovement;
-  [SerializeField] private float _alignSpeed = 12;
-  [Range(0, 180)][SerializeField] private float _snapInsteadAngle = 5;
+  [SerializeField] private float _alignSpeed = 0.5f;
+  [Range(0, 180)][SerializeField] private float _snapInsteadLessThanAngle = 15;
   [SerializeField] private Movement _movementScript;
   [SerializeField] private Transform _xMouseMovementTransform;
   [SerializeField] private Transform _yMouseMovementTransform;
   [Header("Controls")]
   [SerializeField] private float _mouseSensitivity = 30;
-
-  public float _smoothingFactor;
-  private float _currX, _currY;
+  
   private float _desX, _desY;
 
   private void RotateMouseTransforms() {
     _desX += Input.GetAxisRaw("Mouse X") * _mouseSensitivity * Time.deltaTime;
     _desY -= Input.GetAxisRaw("Mouse Y") * _mouseSensitivity * Time.deltaTime;
-    _currX = Mathf.Lerp(_currX, _desX, _smoothingFactor * Time.deltaTime);
-    _currY = Mathf.Lerp(_currY, _desY, _smoothingFactor * Time.deltaTime);
-    _xMouseMovementTransform.transform.localRotation = Quaternion.Euler(0, _currX, 0);
-    _yMouseMovementTransform.transform.localRotation = Quaternion.Euler(_currY, 0, 0);
+    _xMouseMovementTransform.transform.localRotation = Quaternion.Euler(0, _desX, 0);
+    _yMouseMovementTransform.transform.localRotation = Quaternion.Euler(_desY, 0, 0);
   }
   
   private Vector3 GetInputVector() {
@@ -40,27 +36,14 @@ public class PlayerControl : MonoBehaviour
     }
     return _xMouseMovementTransform.TransformDirection(rawDirection);
   }
-
-  private float ConvertTo360(float angle) {
-    if (angle < 0) {
-      return angle + 360;
-    }
-    else {
-      return angle;
-    }
-  }
-  
+   
   private void AlignToMovement() {
     Quaternion target = Quaternion.LookRotation(GetInputVector(), Vector3.up);
     Quaternion current = _transformAlignToMovement.transform.rotation;
-    float targetY = ConvertTo360(target.eulerAngles.y);
-    float currentY = ConvertTo360(current.eulerAngles.y);
-    if (targetY - currentY > _snapInsteadAngle) {
-      // Debug.Log($"Lerped: {currentY}, {targetY}");
-      _transformAlignToMovement.transform.rotation = Quaternion.Slerp(current, target, _alignSpeed);
+    if (Mathf.Abs(Quaternion.Angle(target, current)) > _snapInsteadLessThanAngle) {
+      _transformAlignToMovement.transform.rotation = Quaternion.Lerp(current, target, _alignSpeed);
     }
     else {
-      // Debug.Log($"Snapped: {currentY}, {targetY}");
       _transformAlignToMovement.transform.rotation = target;
     }
   }
@@ -72,8 +55,10 @@ public class PlayerControl : MonoBehaviour
   private void Update() {
     RotateMouseTransforms();
     _movementScript.SetInputVector(GetInputVector());
-    
-    if (GetInputVector() != Vector3.zero) AlignToMovement();
     _movementScript.SetBoolInputs(Input.GetAxisRaw("Jump") > 0, Input.GetAxisRaw("Crouch") > 0);
   }
+  private void FixedUpdate() {
+    if (GetInputVector() != Vector3.zero) AlignToMovement();
+  }
+
 }
