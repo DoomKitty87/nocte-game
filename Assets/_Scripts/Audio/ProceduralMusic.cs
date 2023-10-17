97f344cc77df5e4c8fcb86bbdb2452526f3a5202
DoomKitty87 <doomkitty887@gmail.com>
2023-10-17 11:35:12 -0700

    more instruments and changing tempo + stuff

--- Assets/_Scripts/Audio/ProceduralMusic.cs
+++ Assets/_Scripts/Audio/ProceduralMusic.cs
@@ -17,7 +17,7 @@ public class ProceduralMusic : MonoBehaviour
     public float volume;
     public float frequency;
     public int subdivision;
-    // 0 = 1/16th note, 1 = 1/18th, etc
+    // 0 = 1/16th note, 1 = 1/8th, 2 = 1/4th, 3 = 1/2, 4 = whole, 5 = 2 bars, 6 = 4 bars, 7 = 8 bars, 8 = 16 bars
     public double phase; 
 
     public Instrument(int type) {
@@ -45,11 +45,24 @@ public class ProceduralMusic : MonoBehaviour
   private int _tempo;
   private int _currentScale;
   private int _currentKey;
+  private float _kickVolume = 0.5f;
   // 0 is C, increasing by half-steps
+
+  [SerializeField] private float _tempoChangeProbability = 0.5f;
+  [SerializeField] private int _tempoChangeSubdivision = 7;
+  [SerializeField] private int _tempoChangeVariance = 40;
+  [SerializeField] private int _tempoChangeMean = 100;
+
+  [SerializeField] private int _kickSubdivision = 1;
+  [SerializeField] private float _kickProbability = 0.2f;
 
   private void Awake() {
     _sampleRate = AudioSettings.outputSampleRate;
     SetupInstruments();
+  }
+
+  private void Noise(int sample) {
+    return (noise.snoise(new float2(sample, 0)) + 1) / 2;
   }
 
   private void OnAudioFilterRead(float[] data, int channels) {
@@ -60,11 +73,16 @@ public class ProceduralMusic : MonoBehaviour
         if (_beat[beatSub]) {
           // Do something on subdivisions here
           if (beatSub == 2) {
-            int note =
-                MusicConstants.scales[_currentScale][
-                  (int)((noise.snoise(new float2(sample * 2f, 0)) + 1) / 2 * MusicConstants.scales[_currentScale].Length)] + _currentKey;
-            note %= 11;
+            int note = MusicConstants.scales[_currentScale][(int) (Noise(sample * 2) * MusicConstants.scales[_currentScale].Length)] + _currentKey;
+            note %= 12;
             _instruments[1].frequency = MusicConstants.notes[note];
+          }
+          if (beatSub == _tempoChangeSubdivision) {
+            if (Noise(sample * 5.241f) < _tempoChangeProbability) ChangeTempo((Noise(sample) - 0.5f) * 2 * _tempoChangeVariance + _tempoChangeMean, 2);
+          }
+          if (beatSub == _kickSubdivision) {
+            if (Noise(sample * 234.862f) < _kickProbability) _instruments[0].volume = _kickVolume;
+            else _instruments[0].volume = 0;
           }
           _beat[beatSub] = false;
         }
@@ -89,6 +107,8 @@ public class ProceduralMusic : MonoBehaviour
             break;
           case 3:
             // Synth bass
+            value += SoundPrimitives.leadSynth(_instruments[instrument].phase,
+              _instruments[instrument].volume, 25, _beatPhases[_instruments[instrument].subdivision]);
             break;
           case 4:
             // Hi-hat closed
@@ -113,16 +133,35 @@ public class ProceduralMusic : MonoBehaviour
     }
   }
 
+  private void ChangeTempo(float newTempo, float tempoChangeTime) {
+    StartCoroutine(TempoEase(newTempo, tempoChangeTime));
+  }
+
+  private IEnumerator TempoEase(float newTempo, float transitionTime) {
+    float timer = 0;
+    float initTempo = _tempo;
+    while (timer < transitionTime) {
+      _tempo = Mathf.SmoothStep(initTempo, newTempo, timer / transitionTime);
+      timer += Time.deltaTime;
+      yield return null;
+    }
+    _tempo = newTempo;
+  }
+
   private void SetupInstruments() {
     _currentKey = (int) ((noise.snoise(new float2(DateTime.UtcNow.GetHashCode(), 0)) + 1) / 2 * MusicConstants.notes.Length);
     _currentScale = (int) ((noise.snoise(new float2(0, DateTime.UtcNow.GetHashCode())) + 1) / 2 * MusicConstants.scales.Length);
     _instruments[0].frequency = MusicConstants.notes[_currentKey] / 4;
-    _instruments[0].volume = 0.5f;
-    _instruments[0].subdivision = 2;
+    _instruments[0].volume = _kickVolume;
+    _instruments[0].subdivision = _kickSubdivision;
 
     _instruments[1].frequency = MusicConstants.notes[_currentKey];
     _instruments[1].volume = 0.1f;
     _instruments[1].subdivision = 1;
+
+    _instruments[3].frequency = MusicConstants.notes[_currentKey] / 2;
+    _instruments[3].volume = 0.2f;
+    _instruments[3].subdivision = 4;
     
     _tempo = 120;
     
