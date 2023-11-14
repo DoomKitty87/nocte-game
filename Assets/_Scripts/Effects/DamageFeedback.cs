6f3d39aa5d819f86ccd76c8c43cde4993953ba4b
Matthew Dinh <72724975+feenix1@users.noreply.github.com>
2023-11-13 21:02:52 -0800

    trying damage feedback but can't do until elliot makes camera fp

---
+++ Assets/_Scripts/Effects/DamageFeedback.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageFeedback : MonoBehaviour
+{
+	// What feedback do we want?
+	// Camera shake - Cinemachine
+	// Directional Camera Translation?
+	// Camera Static
+	// Screen Edge Effects - Edge Blurring, Edge "Corruption", Edge Static
+	// Full Screen Effects - Blurring, Static, (Vingette?), Tinting
+	//
+	// What can we do for a "basic" approximation?
+	// Camera Shake
+	// Full Screen Effects - Static, Blurring, Tinting
+
+	// Can't be done until @Elliot makes the camera first person on all scenes
+	
+	// [Header("References")]
+	// [SerializeField] private 
+	//
+	// public void ActivateFeedback(Vector3 damageDirection, float damageIntensityZeroToOne) {
+	// 	
+	// }
+}
