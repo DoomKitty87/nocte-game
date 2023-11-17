9623a5550040174ac8ee75962e0a7aa5104799d8
Elliot <93895160+2Elliot@users.noreply.github.com>
2023-11-17 14:18:38 -0800

    movement corrected for slopes

--- Assets/_Scripts/_Player/PlayerMovementHandler.cs
+++ Assets/_Scripts/_Player/PlayerMovementHandler.cs
@@ -26,8 +26,9 @@ public class PlayerMovementHandler : Subject
 
   private CharacterController _characterController;
 
-  public bool Grounded => IsGrounded();
-  
+  private bool Grounded => IsGrounded();
+
+  public bool OnSlope => IsOnSlope();
   
   private enum MovementState
   {
@@ -50,50 +51,10 @@ public class PlayerMovementHandler : Subject
   private bool IsGrounded() {
     return Physics.CheckSphere(_groundCheck.position, 0.5f, _groundMask);
   }
-  /*
-  private void Update() {
-    UpdateMovementState();
 
-    CalculateGravity();
+  private bool IsOnSlope() {
+    return false;
   }
-
-  private void LateUpdate() {
-    ApplyForce();
-    _characterController.Move(_newVelocity * Time.deltaTime);
-    _accelerationVector = Vector3.zero;
-  }
-
-  private void ApplyForce() {
-    // _velocityVector = _characterController.velocity;
-    // 
-    // // Apply friction if grounded
-    // if (State != "Air") {
-    //   _velocityVector += new Vector3(CalculateFriction(_characterController.velocity.x), 0f,
-    //     CalculateFriction(_characterController.velocity.z));
-    // }
-    // 
-    _velocityVector += _accelerationVector;
-    
-    _newVelocity = _velocityVector;
-    // Debug.Log(_characterController.velocity.magnitude);
-  }
-  
-  public void AddForce(Vector3 forceVector) {
-    _accelerationVector += forceVector;
-  }
-
-  private void CalculateGravity() {
-    if (_characterController.isGrounded)
-      _accelerationVector.y += _groundedGravity * Time.deltaTime;
-    else 
-      _accelerationVector.y += _gravity * Time.deltaTime;
-  }
-  
-  private float CalculateFriction(float magnitude) {
-    float magnitudeWithFriction = magnitude * _frictionCoefficient;
-    return -magnitudeWithFriction * Time.deltaTime;
-  }
-  */
   
   private void UpdateState() {
     if (State != "Freeze" && State != "Grapple") {
@@ -116,67 +77,3 @@ public class PlayerMovementHandler : Subject
     
   }
 }
-
-/*using System;
-using UnityEngine;
-
-using ObserverPattern;
-
-public class PlayerMovementHandler : Subject
-{
-  [SerializeField] MovementState _movementState;
-
-  private string _previousState;
-
-  public string State {
-    get => _movementState.ToString();
-    set => _movementState = Enum.Parse<MovementState>(value, true);
-  }
-
-  [SerializeField] private LayerMask _groundMask;
-
-  [Header("Keybinds")] 
-  public KeyCode _jumpKey = KeyCode.Space;
-  // public KeyCode _sprintKey = KeyCode.LeftShift;
-  public KeyCode _crouchKey = KeyCode.LeftControl;
-  public KeyCode _slideKey = KeyCode.LeftControl;
-
-  private enum MovementState
-  {
-    Freeze,
-    Walking,
-    Sprinting,
-    Crouching,
-    Sliding,
-    Air
-  }
-
-  private void Start() {
-    _previousState = State;
-  }
-
-  private void Update() {
-    UpdateMovementState();
-  }
-
-  private void UpdateMovementState() {
-    if (State != "Freeze") {
-      var groundCheckRay = new Ray(transform.position + (Vector3.down * 0.6f), Vector3.down);
-      if (!Physics.SphereCast(groundCheckRay, 0.25f, 0.65f, _groundMask))
-        State = "Air";
-      else if (Input.GetKey(_slideKey))
-        State = "Sliding";
-      //else if (Input.GetKey(_sprintKey))
-      //  State = "Sprinting";
-      else if (Input.GetKey(_crouchKey))
-        State = "Crouching";
-      else
-        State = "Sprinting";
-    }
-
-    if (_previousState == State) return;
-    NotifyObservers(_previousState, State);
-    _previousState = State;
-    
-  }
-}*/
