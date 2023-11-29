87d33f2fb64dd7ee4d2f4c2db429354ba739fc1f
DoomKitty87 <doomkitty887@gmail.com>
2023-11-29 14:53:14 -0800

    nvmd im dumb i think this is fixed

--- Assets/_Scripts/Effects/OrbitVisualizer.cs
+++ Assets/_Scripts/Effects/OrbitVisualizer.cs
@@ -11,17 +11,10 @@ public class OrbitVisualizer : MonoBehaviour
 
   public void Initialize() {
     for (int i = 0; i < _moonManager.moons.Length; i++) {
-      Vector3[] positions = new Vector3[Mathf.Pow(2, _resolution + 1) * 4];
-      positions[0] = Quaternion.LookRotation(_moonManager._moons[i].orbitAxis) * new Vector3(1, 0, 0) * (_moonManager._moons[i].distance);
-      positions[1] = Quaternion.LookRotation(_moonManager._moons[i].orbitAxis) * new Vector3(0, 1, 0) * (_moonManager._moons[i].distance);
-      positions[2] = Quaternion.LookRotation(_moonManager._moons[i].orbitAxis) * new Vector3(-1, 0, 0) * (_moonManager._moons[i].distance);
-      positions[3] = Quaternion.LookRotation(_moonManager._moons[i].orbitAxis) * new Vector3(0, -1, 0) * (_moonManager._moons[i].distance);
-      int n = 3;
-      for (int j = 1; j < _resolution; j++) {
-        for (int k = 0, c = n; k < c; k++) {
-          positions[n] = (positions[k] + positions[k == c - 1 ? 0 : k + 1]) / 2;
-          n++;
-        }
+      Vector3[] positions = new Vector3[_resolution];
+      for (int j = 0; j < _resolution; j++) {
+        float radians = Mathf.PI * 2 / _resolution * (j + 1);
+        positions[j] = Quaternion.LookRotation(_moonManager._moons[i].orbitAxis) * new Vector3(Mathf.Cos(radians), Mathf.Sin(radians), 0) * (_moonManager._moons[i].distance);
       }
       _lineRenderers[i].SetPositions(positions);
     }
