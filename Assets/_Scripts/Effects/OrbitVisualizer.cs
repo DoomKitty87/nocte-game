16c7a084a7f500052bf104c82367299945c9bd17
Ansel Stanik <ansel.stanik@icloud.com>
2023-11-30 09:14:51 -0800

    finished orbit visualizer

--- Assets/_Scripts/Effects/OrbitVisualizer.cs
+++ Assets/_Scripts/Effects/OrbitVisualizer.cs
@@ -13,9 +13,11 @@ public class OrbitVisualizer : MonoBehaviour
     for (int i = 0; i < _moonManager._moons.Length; i++) {
       Vector3[] positions = new Vector3[_resolution];
       for (int j = 0; j < _resolution; j++) {
-        float radians = Mathf.PI * 2 / _resolution * (j + 1);
-        positions[j] = Quaternion.LookRotation(_moonManager._moons[i].orbitAxis) * new Vector3(Mathf.Cos(radians), Mathf.Sin(radians), 0) * (_moonManager._moons[i].distance);
+        float radians = Mathf.PI * 2 / _resolution * j;
+        positions[j] = Quaternion.LookRotation(_moonManager._moons[i].orbitAxis) * new Vector3(Mathf.Cos(radians), Mathf.Sin(radians), 0) / transform.parent.localScale.x * _moonManager._moons[i].distance;
       }
+
+      _lineRenderers[i].positionCount = positions.Length;
       _lineRenderers[i].SetPositions(positions);
     }
   }
