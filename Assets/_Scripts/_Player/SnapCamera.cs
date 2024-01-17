b84fe308cf9221ac5d570d8a220160c412029d48
Matthew Dinh <72724975+help-programming-is-hard@users.noreply.github.com>
2024-01-17 09:44:11 -0800

    changes to player prefab, refs added to playermain.blend

--- Assets/_Scripts/_Player/SnapCamera.cs
+++ Assets/_Scripts/_Player/SnapCamera.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 public class SnapCamera : MonoBehaviour
 {
     [SerializeField] private Transform _head;
-    
+
+    private void OnValidate() {
+        transform.position = _head.transform.position;
+    }
+
     private void Update() {
         transform.position = _head.transform.position;
     }
