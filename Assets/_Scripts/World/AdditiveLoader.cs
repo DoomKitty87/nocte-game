681a8e22add9af40ea55d70d37d9656bc323b98f
Elliot <ejdalessa@gmail.com>
2024-01-22 09:59:30 -0800

    working on additive loading

---
+++ Assets/_Scripts/World/AdditiveLoader.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdditiveLoader : MonoBehaviour
+{
+  private void OnTriggerEnter(Collider other) {
+    if (other.CompareTag("Player")) Debug.Log("Enter");
+  }
+
+  private void OnTriggerExit(Collider other) {
+    if (other.CompareTag("Player")) Debug.Log("Exit");
+  }
+}
