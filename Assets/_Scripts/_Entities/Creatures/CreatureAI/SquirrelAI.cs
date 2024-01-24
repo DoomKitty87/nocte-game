68c7d3e61a51acaff82621ed67a0d4a8e4b05ef6
SpikyLlama <62197004+SpikyLlama@users.noreply.github.com>
2024-01-24 09:59:22 -0800

    made squirrel ai that mostly works!

--- Assets/_Scripts/_Entities/Creatures/CreatureAI/SquirrelAI.cs
+++ Assets/_Scripts/_Entities/Creatures/CreatureAI/SquirrelAI.cs
@@ -11,16 +11,16 @@ namespace _Scripts._Entities.Creatures.CreatureAI
 	// Define leaf nodes here
 	[RequireComponent(typeof(Transform))]
 	[RequireComponent(typeof(CreatureCombat))]
-	public class HostileAI : BehaviorTree
+	public class SquirrelAI : BehaviorTree
 	{
-		[Header("References")] private Transform _transform;
+		[Header("References")] 
+		private Transform _transform;
 		private CreatureCombat _creatureCombat;
 		private EnemyController _controller;
 		[SerializeField] private Transform _playerTransform;
 
 		[Header("Settings")] 
-		[SerializeField] private float _distanceAttack;
-		[SerializeField] private float _distanceChase;
+		[SerializeField] private float _distanceEvade;
 		[SerializeField] private float _distanceGoal;
 		[SerializeField] private float _range;
 		[SerializeField] private List<CreatureAttack> _attacks;
@@ -31,19 +31,14 @@ namespace _Scripts._Entities.Creatures.CreatureAI
 			_creatureCombat = gameObject.GetComponent<CreatureCombat>();
 			return new Noder(new List<TreeNode> {
 				new Selector(new List<TreeNode> {
-					new Sequencer (new List<TreeNode> {
-						new DistanceTransformLessThan(_transform, _playerTransform, _distanceAttack),
-						new SetState(_controller, EnemyController.PlayerStates.Idle),
-						//new UseAttack(_creatureCombat, _attacks)
-					}),
 					new Sequencer(new List<TreeNode> {  // checks if the enemy is close to the player and starts chasing
-						new DistanceTransformLessThan(_transform, _playerTransform, _distanceChase),
-						new FaceTransform(_transform, _playerTransform, true, false, true),
+						new DistanceTransformLessThan(_transform, _playerTransform, _distanceEvade),
+						new FaceTransform(_transform, _playerTransform, true, false, true, true),
 						new SetMovementToDirection(_transform.forward, _controller),
 						new SetState(_controller, EnemyController.PlayerStates.Sprinting),
 					}), 
 					new Sequencer(new List<TreeNode> { // checks if the enemy is far from the player and starts roaming
-						new Invertor(new DistanceTransformLessThan(_transform, _playerTransform, _distanceChase)),
+						new Invertor(new DistanceTransformLessThan(_transform, _playerTransform, _distanceEvade)),
 						new Invertor(new CheckGoalExists(_controller)),
 						new PlaceRandomGoal(_controller, _transform, _range),
 						new SetState(_controller, EnemyController.PlayerStates.Walking),
@@ -55,7 +50,7 @@ namespace _Scripts._Entities.Creatures.CreatureAI
 					new PlaceRandomGoal(_controller, _transform, _range)
 				}),
 				new Sequencer(new List<TreeNode> { // checks if the enemy is far from the goal and makes him move closer
-					new Invertor(new DistanceTransformLessThan(_transform, _playerTransform, _distanceChase)),
+					new Invertor(new DistanceTransformLessThan(_transform, _playerTransform, _distanceEvade)),
 					new SetState(_controller, EnemyController.PlayerStates.Walking),
 					new CheckGoalExists(_controller),
 					new FaceGoal(_controller, _transform, true, false, true),
