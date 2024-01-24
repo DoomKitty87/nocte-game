8ec3d5539b1ecee1bef6e06ab22fb1a1014e33f3
Ansel Stanik <ansel.stanik@icloud.com>
2024-01-24 09:58:03 -0800

    fixed some problems

--- Assets/_Scripts/Generation/PlaceStructures.cs
+++ Assets/_Scripts/Generation/PlaceStructures.cs
@@ -23,11 +23,13 @@ public class PlaceStructures : MonoBehaviour
   [SerializeField] private float _roadWidth;
   [SerializeField] private float _roadDepth;
   [SerializeField] private float _roadInset;
+  [SerializeField] private float _roadBevel;
   [SerializeField] private float _roadWaterImpact;
   [SerializeField] private float _roadNoiseAmplitude;
   [SerializeField] private Material _roadMaterial;
 
   private Vector3[] _structurePositions;
+  private List<Transform> _structures = new List<Transform>();
 
   private int[] _beamWindingOrder = {
     1, 2, 0, 1, 3, 2, // Bottom Face
@@ -75,6 +77,7 @@ public class PlaceStructures : MonoBehaviour
     if (heighth > maxHeight) maxHeight = heighth;
     go.transform.position = new Vector3(go.transform.position.x, maxHeight, go.transform.position.z);
     go.transform.parent = transform;
+    _structures.Add(go.transform);
     mainPosition = go.transform.position;
 
     List<Vector3> vertices = new List<Vector3>();
@@ -154,7 +157,7 @@ public class PlaceStructures : MonoBehaviour
         roadVertexColors[j + roadPlane.Length] = new Color(_worldGen.GetRiverValue(roadPlane[j]), 0, 0, 0);
       }
 
-      Mesh road = RoadGenerator.MeshFromPlane(roadPlane3, _roadDepth, _roadInset);
+      Mesh road = RoadGenerator.MeshFromPlane(roadPlane3, _roadDepth, _roadInset, _roadBevel);
       road.colors = roadVertexColors;
       GameObject obj = new GameObject();
       obj.layer = 6;
@@ -230,6 +233,7 @@ public class PlaceStructures : MonoBehaviour
       }
 
       go.transform.parent = transform;
+      _structures.Add(go.transform);
     }
   
     //for (int i = 0; i < _structurePositions.Length; i++) {
@@ -257,9 +261,9 @@ public class PlaceStructures : MonoBehaviour
   }
 
   public void CheckStructures(Vector2 playerPosition) {
-    for (int i = 0; i < transform.childCount; i++) {
-      if ((new Vector2(transform.GetChild(i).position.x, transform.GetChild(i).position.z) - playerPosition).magnitude > _structureRenderDistance) transform.GetChild(i).gameObject.SetActive(false);
-      else transform.GetChild(i).gameObject.SetActive(true);
+    for (int i = 0; i < _structures.Count; i++) {
+      if ((new Vector2(_structures[i].position.x, _structures[i].position.z) - playerPosition).magnitude > _structureRenderDistance) _structures[i].gameObject.SetActive(false);
+      else _structures[i].gameObject.SetActive(true);
     }
   }
 
--- Assets/_Scripts/Generation/WorldGenerator.cs
+++ Assets/_Scripts/Generation/WorldGenerator.cs
@@ -833,8 +833,8 @@ public class WorldGenerator : MonoBehaviour
 
     for (int i = 0; i < heightMods.Length; i++) heightMods[i] = _riverParameters.noiseCurve.Evaluate(heightMods[i]) * _riverParameters.heightCurve.Evaluate(vertices[i].y / _maxPossibleHeight) * _riverParameters.normalCurve.Evaluate(Mathf.Abs(normals[i].y));
 
-    NativeArray<Vector3> waterVertsFinal = new NativeArray<Vector3>(0, Allocator.TempJob);
-    NativeArray<int> waterTrisFinal = new NativeArray<int>(0, Allocator.TempJob);
+    NativeList<Vector3> waterVertsFinal = new NativeList<Vector3>(0, Allocator.TempJob);
+    NativeList<int> waterTrisFinal = new NativeList<int>(0, Allocator.TempJob);
 
     RiverPassJob riverPassJob = new RiverPassJob {
       vertices = vertices,
@@ -857,8 +857,8 @@ public class WorldGenerator : MonoBehaviour
     _tilePool[index].waterVertCount = waterVertsFinal.Length;
     _tilePool[index].waterTriIndex = _waterTriangles.Count;
     _tilePool[index].waterTriCount = waterTrisFinal.Length;
-    _waterVertices.AddRange(waterVertsFinal);
-    _waterTriangles.AddRange(waterTrisFinal);
+    _waterVertices.AddRange(waterVertsFinal.ToArray());
+    _waterTriangles.AddRange(waterTrisFinal.ToArray());
     waterVertsFinal.Dispose();
     waterTrisFinal.Dispose();
     heightMods.Dispose();
@@ -914,8 +914,8 @@ public class WorldGenerator : MonoBehaviour
     [ReadOnly] public int waterVertCount;
     [ReadOnly] public NativeArray<int> triangles;
 
-    [WriteOnly] public NativeArray<Vector3> waterVertsOut;
-    [WriteOnly] public NativeArray<int> waterTrisOut;
+    [WriteOnly] public NativeList<Vector3> waterVertsOut;
+    [WriteOnly] public NativeList<int> waterTrisOut;
 
     public void Execute() {
       Vector3[] waterVerts = new Vector3[vertices.Length];
@@ -955,7 +955,7 @@ public class WorldGenerator : MonoBehaviour
       }
 
       int[] realIndices = new int[waterVerts.Length];
-      waterVertsOut = new NativeArray<Vector3>(waterVerts.Length - ignored, Allocator.Temp);
+      waterVertsOut.Length = waterVerts.Length - ignored;
       for (int i = 0, j = 0; i < waterVerts.Length; i++) {
         if (!ignoreVerts[i]) {
           waterVertsOut[j] = waterVerts[i];
@@ -969,7 +969,7 @@ public class WorldGenerator : MonoBehaviour
           triCount += 3;
         }
       }
-      waterTrisOut = new NativeArray<int>(triCount, Allocator.Temp);
+      waterTrisOut.Length = triCount;
       for (int i = 0, j = 0; i < triangles.Length; i+= 3) {
         if (!ignoreVerts[waterTriangles[i]] && !ignoreVerts[waterTriangles[i + 1]] && !ignoreVerts[waterTriangles[i + 2]]) {
           waterTrisOut[j] = realIndices[waterTriangles[i]] + waterVertCount;
