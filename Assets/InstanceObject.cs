0830c9f8596712aeb2f42fc50a550c22b5c69c43
Elliot <93895160+2Elliot@users.noreply.github.com>
2024-02-10 14:52:32 -0800

    GPU instancing

--- Assets/InstanceObject.cs
+++ Assets/InstanceObject.cs
@@ -6,32 +6,60 @@ using UnityEngine;
 public class InstanceObject : MonoBehaviour
 {
     [SerializeField] private GameObject _testPrefab;
-    private Vector2[] positions;
+    private Vector3[] positions;
     public int numberOfInstancesInOneDirection;
     public float distanceBetweenInstance;
     public float randomLocationOffset;
 
+    public bool GPUInstance;
+
+    public Material material;
+    public Mesh mesh;
+
     private WorldGenInfo.AmalgamNoiseParams _noiseParams;
 
     private void Awake() {
-        positions = new Vector2[numberOfInstancesInOneDirection * numberOfInstancesInOneDirection];
+        positions = new Vector3[numberOfInstancesInOneDirection * numberOfInstancesInOneDirection];
 
         int currentInstance = 0;
         for (int i = 0; i < numberOfInstancesInOneDirection; i++) {
             for (int j = 0; j < numberOfInstancesInOneDirection; j++) {
-                positions[currentInstance] = new Vector2(i * distanceBetweenInstance, j * distanceBetweenInstance) + new Vector2(UnityEngine.Random.Range(-randomLocationOffset / 2, randomLocationOffset), UnityEngine.Random.Range(-randomLocationOffset / 2, randomLocationOffset));
+                float xPosition = i * distanceBetweenInstance + UnityEngine.Random.Range(
+                    -randomLocationOffset / 2,
+                    randomLocationOffset / 2
+                );
+                float zPosition = j * distanceBetweenInstance + UnityEngine.Random.Range(
+                    -randomLocationOffset / 2, 
+                    randomLocationOffset / 2
+                );
+                float yPosition = AmalgamNoise.GetPosition(xPosition, zPosition);
+                positions[currentInstance] = new Vector3(xPosition, yPosition, zPosition);
                 currentInstance++;
             }
         }
     }
 
     private void Start() {
-        GameObject parentObject = new GameObject();
-        parentObject.name = "Foliage Holder";
+        if (GPUInstance) return;
+        GameObject parentObject = new GameObject {
+            name = "Foliage Holder"
+        };
         Instantiate(parentObject);
-        
-        for (int i = 0; i < positions.Length; i++) {
-            Instantiate(_testPrefab, new Vector3(positions[i].x, AmalgamNoise.GetPosition(positions[i].x, positions[i].y), positions[i].y), Quaternion.identity, parent: parentObject.transform);
+
+        foreach (var t in positions) {
+            Instantiate(
+                _testPrefab, t,
+                Quaternion.identity,
+                parent: parentObject.transform
+            );
         }
+    }
+
+    private void Update() {
+        if (!GPUInstance) return;
+        RenderParams rp = new RenderParams(material);
+        Matrix4x4[] instData = new Matrix4x4[positions.Length];
+        for (int i = 0; i < positions.Length; i++) instData[i] = Matrix4x4.Translate(positions[i]);
+        Graphics.RenderMeshInstanced(rp, mesh, 0, instData);
     }
 }
