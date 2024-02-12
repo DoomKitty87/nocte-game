4a22177b5e72ad6ade72587bfed5e23015c12e26
Elliot <ejdalessa@gmail.com>
2024-02-12 09:59:16 -0800

    stuff doesnt work

--- Assets/_Scripts/InstanceObjects.cs
+++ Assets/_Scripts/InstanceObjects.cs
@@ -5,6 +5,9 @@ using UnityEngine.Serialization;
 
 public class RenderInstancedObjects : MonoBehaviour
 {
+    public Transform _playerTransform;
+    private Vector2 _playerPositionXZ;
+    
     private InstanceObjects _instanceObjects;
 
     private void Awake() {
@@ -14,10 +17,12 @@ public class RenderInstancedObjects : MonoBehaviour
 
     // Temporary test case
     private void Start() {
-        _instanceObjects.AddObject(new Vector2(0, 0), FoliageType.Tree);
+        _instanceObjects.AddObject(new Vector2(0, 0), FoliageType.TestObject);
     }
 
     private void Update() {
+        _playerPositionXZ = new Vector2(_playerTransform.position.x, _playerTransform.position.z);
+        
         int numberOfChunks = _instanceObjects.ChunksDictionary.Count;
         int numberOfTypes = _instanceObjects.FoliageDataDictionary.Count;
 
@@ -26,27 +31,66 @@ public class RenderInstancedObjects : MonoBehaviour
             // For each type in given chunk
             foreach (List<FoliageData> type in chunk.FoliageTypePerChunk.Values) {
                 // For each type of foliage in given chunk
-                RenderObject(type);
+                RenderObject(chunk, type);
             }
         }
     }
 
-    private void RenderObject(List<FoliageData> type) {
-        int numberOfObjects = type.Count;
+    private void RenderObject(FoliageChunk chunk, List<FoliageData> typeOfFoliage) {
+        FoliageData firstData = typeOfFoliage[0];
+        FoliageType type = firstData.Type;
+        FoliageMetaData metaData = _instanceObjects.GetFoliageMetaData(type);
         
-        RenderParams rp = new RenderParams(material);
+        // TODO: Optimize this by precalculating number of possible LOD in chunk
+        int numberOfLODRanges = _instanceObjects.GetFoliageMetaData(firstData.Type)._lodData.Count;
+        float[] lodRanges = new float[numberOfLODRanges];
+        List<FoliageData>[] lodLevels = new List<FoliageData>[numberOfLODRanges];
+
+        BuildLODRanges(metaData, numberOfLODRanges, ref lodRanges);
+        
+        foreach (FoliageData data in typeOfFoliage) {
+            int lodLevel = GetLODRange(data.Position, lodRanges);
+            lodLevels[lodLevel].Add(data);
+        }
+
+        foreach (float f in lodRanges) {
+            Debug.Log(f);
+        }
+
+        // Destroy(this);
+
+        // RenderParams rp = new RenderParams(material);
+        /*
         Matrix4x4[] instData = new Matrix4x4[numberOfObjects];
-        
+
         for (int i = 0; i < numberOfObjects; i++) {
             Mesh mesh = _instanceObjects.GetFoliageData(type[i].Type).GetLODData(0).Item1;
             Material material = _instanceObjects.GetFoliageData(type[i].Type).GetLODData(0).Item2;
             Vector3 position = new Vector3(type[i].Position.x, AmalgamNoise.GetPosition(type[i].Position), type[i].Position.y);
-            
+
 
             instData[i] = Matrix4x4.Translate(position);
         }
+        */
+
+        // Graphics.RenderMeshInstanced(rp, mesh, 0, instData);
+    }
+
+    private void BuildLODRanges(FoliageMetaData data, int numberOfLODRanges, ref float[] array) {
+        for (int i = 0; i < numberOfLODRanges; i++) {
+            array[i] = data._lodData[i]._lodRange;
+        }
+    }
+
+    private int GetLODRange(Vector2 pos, float[] ranges) {
+        float distance = Vector2.Distance(_playerPositionXZ, pos);
         
-        Graphics.RenderMeshInstanced(rp, mesh, 0, instData);
+        // Searches for LOD range, if none are found then defaults to last range.
+        for (int i = 0; i < ranges.Length - 1; i++) {
+            if (distance < ranges[i]) return i;
+        }
+
+        return ranges.Length;
     }
 }
 
@@ -102,7 +146,7 @@ public class InstanceObjects
         }
     }
 
-    public FoliageMetaData GetFoliageData(FoliageType type) {
+    public FoliageMetaData GetFoliageMetaData(FoliageType type) {
         if (FoliageDataDictionary.TryGetValue(type, out var data)) {
             return data;
         }
@@ -138,7 +182,8 @@ public enum FoliageType
 {
     Tree,
     Shrub,
-    Bush
+    Bush,
+    TestObject
     // Add more types here
 }
 
@@ -174,23 +219,29 @@ public class FoliageData
 [CreateAssetMenu(fileName = "FoliageMetaData", menuName = "ScriptableObjects/FoliageMetaData")]
 public class FoliageMetaData : ScriptableObject
 {
-    [HideInInspector] public FoliageType _type;
+    public FoliageType _type;
     
     [Serializable]
     public class LODData
     {
         public int _lodLevel;
+        public float _lodRange;
         public Mesh _mesh;
         public Material _material;
     }
 
-    public List<LODData> _lodData = new List<LODData>();
+    public Dictionary<int, LODData> _lodData = new Dictionary<int, LODData>();
     
-    public (Mesh, Material) GetLODData(int lodLevel)
-    {
-        LODData lodMesh = _lodData.Find(mesh => mesh._lodLevel == lodLevel);
-        LODData lodMat = _lodData.Find(material => material._lodLevel == lodLevel);
+    public (Mesh, Material) GetLODData(int lodLevel) {
+        LODData data = _lodData[lodLevel];
         
-        return ((lodMesh != null && lodMat != null) ? (mesh: lodMesh._mesh, material: lodMat._material) : (null, null));
+        Mesh lodMesh = data._mesh;
+        Material lodMat = data._material;
+        
+        return ((lodMesh != null && lodMat != null) ? (mesh: lodMesh, material: lodMat) : (null, null));
+    }
+
+    public float GetRange(int lodLevel) {
+        return _lodData[lodLevel]._lodRange;
     }
 }
