116ef00eb56833321cb1c04019e95f75b88661ee
Matthew Dinh <72724975+help-programming-is-hard@users.noreply.github.com>
2024-02-24 09:49:57 -0800

    added grass back into world generator

--- Assets/_Scripts/Generation/WorldGenerator.cs
+++ Assets/_Scripts/Generation/WorldGenerator.cs
@@ -535,8 +535,8 @@ public class WorldGenerator : MonoBehaviour
     // If you need to put anything else (tag, components, etc) on the tile, do it here. If it needs to change every time the LOD is changed, do it in the UpdateTile function.
     go.tag = "Ground";
     go.layer = 6;
-    // GrassTilePrimitives grass = go.AddComponent<GrassTilePrimitives>();
-    // grass._worldGenerator = this;
+    GrassTilePrimitives grass = go.AddComponent<GrassTilePrimitives>();
+    grass._worldGenerator = this;
     _structures.GenerateChunkStructures(new Vector2(x * _size * _resolution, z * _size * _resolution), new Vector2((x + 1) * _size * _resolution, (z + 1) * _size * _resolution));
     _generatedStructureTiles.Add(new Vector2(x, z));
     tile.mesh = msh;
