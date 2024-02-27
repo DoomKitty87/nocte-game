ee09cb9f92bc2204c95fbe7f0a55c1acb9354f6c
SpikyLlama <62197004+SpikyLlama@users.noreply.github.com>
2024-02-27 09:58:53 -0800

    got ecs going. i lov  unity ecs ecs ecs ecs ecs ecs ecs ecs ecs ecs ecs ecs

---
+++ Assets/_Scripts/_Entities/EnemyManager.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public struct BirdsManager : IEquatable<EntityManager>
+{
+  public bool Equals(EntityManager other) {
+    throw new NotImplementedException();
+  }
+  
+  
+}
