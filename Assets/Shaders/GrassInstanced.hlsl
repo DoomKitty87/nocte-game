0f84f3bb9176f2d70236878fc1b90bb48634eb8a
DoomKitty87 <doomkitty887@gmail.com>
2024-03-07 19:41:04 -0800

    fixed grass instancing shader kinda

--- Assets/Shaders/GrassInstanced.hlsl
+++ Assets/Shaders/GrassInstanced.hlsl
@@ -1,12 +1,67 @@
 #ifndef GRASS_INSTANCED_INCLUDED
 #define GRASS_INSTANCED_INCLUDED
 
+#define unity_ObjectToWorld unity_ObjectToWorld
+#define unity_WorldToObject unity_WorldToObject
+
 StructuredBuffer<float4> _instancePositions;
+
+float randomRange(float2 seed, float min, float max)
+{
+  const float randNum = frac(sin(dot(seed, float2(12.9898, 78.233)))*143758.5453);
+  return lerp(min, max, randNum);
+}
+
+float3x3 AngleAxis3x3(float angle, float3 axis)
+{
+  float c, s;
+  sincos(angle, s, c);
+
+  float t = 1 - c;
+  float x = axis.x;
+  float y = axis.y;
+  float z = axis.z;
+
+  return float3x3(
+      t * x * x + c,      t * x * y - s * z,  t * x * z + s * y,
+      t * x * y + s * z,  t * y * y + c,      t * y * z - s * x,
+      t * x * z - s * y,  t * y * z + s * x,  t * z * z + c
+  );
+}
 
 #if UNITY_ANY_INSTANCING_ENABLED
 
-  void vertInstancingSetup(){
-  }
+  void vertInstancingMatrices(inout float4x4 objectToWorld, out float4x4 worldToObject) {
+		float4 data = _instancePositions[unity_InstanceID];
+
+    float3 wPos = data.xyz;
+
+    float3x3 rot = AngleAxis3x3(data.w, float3(0, 1, 0));
+
+    objectToWorld._11_21_31_41 = float4(rot[0], 0.0f);
+    objectToWorld._12_22_32_42 = float4(rot[1], 0.0f);
+    objectToWorld._13_23_33_43 = float4(rot[2], 0.0f);
+    objectToWorld._14_24_34_44 = float4(wPos, 1.0f);
+
+    float3x3 w2oRotation;
+		w2oRotation[0] = objectToWorld[1].yzx * objectToWorld[2].zxy - objectToWorld[1].zxy * objectToWorld[2].yzx;
+		w2oRotation[1] = objectToWorld[0].zxy * objectToWorld[2].yzx - objectToWorld[0].yzx * objectToWorld[2].zxy;
+		w2oRotation[2] = objectToWorld[0].yzx * objectToWorld[1].zxy - objectToWorld[0].zxy * objectToWorld[1].yzx;
+
+		float det = dot(objectToWorld[0].xyz, w2oRotation[0]);
+		w2oRotation = transpose(w2oRotation);
+		w2oRotation *= rcp(det);
+		float3 w2oPosition = mul(w2oRotation, -objectToWorld._14_24_34);
+
+		worldToObject._11_21_31_41 = float4(w2oRotation._11_21_31, 0.0f);
+		worldToObject._12_22_32_42 = float4(w2oRotation._12_22_32, 0.0f);
+		worldToObject._13_23_33_43 = float4(w2oRotation._13_23_33, 0.0f);
+		worldToObject._14_24_34_44 = float4(w2oPosition, 1.0f);
+	}
+
+	void vertInstancingSetup() {
+		vertInstancingMatrices(unity_ObjectToWorld, unity_WorldToObject);
+	}
 
 #endif
 
@@ -19,26 +74,11 @@ void GetInstanceID_float(out float Out){
 	#endif
 }
 
-float randomRange(float2 seed, float min, float max)
-{
-  const float randNum = frac(sin(dot(seed, float2(12.9898, 78.233)))*143758.5453);
-  return lerp(min, max, randNum);
-}
-
-float4 RotateAroundYInDegrees (float4 vertex, float rads) {
-  float sina, cosa;
-  sincos(rads, sina, cosa);
-  float2x2 m = float2x2(cosa, -sina, sina, cosa);
-  return float4(mul(m, vertex.xz), vertex.yw).xzyw;
-}
-
 void Instancing_float(float3 Position, out float3 Out, out float3 World){
+  Out = Position;
   float id = 0;
   GetInstanceID_float(id);
-  float3 pos = _instancePositions[(int) id].xyz;
-  Position = RotateAroundYInDegrees(float4(Position, 0), randomRange(pos.xz, -3.14, 3.14)).xyz;
-      Out = Position + pos;
-      World = pos;
+  World = _instancePositions[id].xyz;
 }
 
 #endif
