9faa6b4efe11d3d4490fdbd0c01cb6c1198629d2
Ansel Stanik <ansel.stanik@icloud.com>
2024-03-14 08:32:58 -0700

    fixed bad culling

--- Assets/_Scripts/Effects/IndirectGrass/RenderChunk.cs
+++ Assets/_Scripts/Effects/IndirectGrass/RenderChunk.cs
@@ -92,7 +92,7 @@ public class RenderChunk {
 
   public void Render(Vector2 cameraPosition, Camera camera) {
 
-    if (camera.WorldToScreenPoint(new Vector3(_position.x, 0, _position.z)).z < 0) {
+    if (camera.WorldToScreenPoint(new Vector3(_position.x, camera.transform.position.y, _position.z)).z < -_chunkSize) {
       return;
     }
 
