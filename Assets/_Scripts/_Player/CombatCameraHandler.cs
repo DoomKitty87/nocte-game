821b998414acbadfb552d011cba81d2e211caaa7
Matthew Dinh <72724975+help-programming-is-hard@users.noreply.github.com>
2024-03-24 10:46:35 -0700

    fixed compile error

--- Assets/_Scripts/_Player/CombatCameraHandler.cs
+++ Assets/_Scripts/_Player/CombatCameraHandler.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CombatCameraHandler : MonoBehaviour
 {
-    [SerializeField] private Cinemachine
+    // [SerializeField] private Cinemachine
     // Start is called before the first frame update
     void Start()
     {
