ead85551c73151ba2ed07f9628ae7a8655183e7a
DoomKitty87 <doomkitty887@gmail.com>
2024-03-26 18:53:16 -0700

    finished scene stuff

---
+++ Assets/_Scripts/UI/ShipMenuNavigation.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMenuNavigation : MonoBehaviour
+{
+
+  public void EnterGame() {
+    SceneHandler.Instance.EnterGame();
+  }
+
+}
