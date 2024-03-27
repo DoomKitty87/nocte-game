7e12e8e837c66f446d3c86d3676443e088362376
DoomKitty87 <doomkitty887@gmail.com>
2024-03-26 22:39:31 -0700

    setup menu navigations

---
+++ Assets/_Scripts/UI/MainMenuActions.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuActions : MonoBehaviour
+{
+
+  [SerializeField] private string _shipScene;
+
+  public void ExitGame() {
+    Application.Quit();
+  }
+
+  public void StartGame() {
+    SceneManager.LoadScene(_shipScene);
+  }
+
+  public void OpenOptions() {
+    Debug.Log("Opening options");
+  }
+
+}
