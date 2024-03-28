367afedcf19bca7982c4dcfe1817a5d508b832c7
Elliot <93895160+2Elliot@users.noreply.github.com>
2024-03-27 23:09:28 -0700

    works kinda sorta better

--- Assets/_Scripts/Progression/ProgressionStorage.cs
+++ Assets/_Scripts/Progression/ProgressionStorage.cs
@@ -2,48 +2,10 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public class ProgressionStorage : MonoBehaviour
+public class ProgressionStorage0 : MonoBehaviour
 {
 
-  [System.Serializable]
-  public struct ProgressionData
-  {
-    public int cores;
-    public int usedcores;
-    public bool[] ownedBlueprints;
-    public bool[] unlockedBlueprints;
 
-  }
 
-  public ProgressionData _progression = new ProgressionData();
 
-  private static ProgressionStorage _instance;
-  public static ProgressionStorage Instance { get { return _instance; } }
-
-  private void Awake() {
-    if (_instance == null) {
-      _instance = this;
-    } else {
-      Destroy(this);
-    }
-  }
-
-  public void SaveProgressionData()
-  {
-    StorageInterface.SaveData("progression.dat", _progression);
-  }
-
-  public ProgressionData LoadProgressionData()
-  {
-    if (StorageInterface.LoadData("progression.dat") == null) {
-      _progression.cores = 0;
-      _progression.usedcores = 0;
-      _progression.ownedBlueprints = new bool[5] {true, true, false, false, true};
-      _progression.unlockedBlueprints = new bool[5] {true, true, false, false, true};
-      return _progression;
-    }
-    ProgressionData data = (ProgressionData)StorageInterface.LoadData("progression.dat");
-    _progression = data;
-    return data;
-  }
 }
