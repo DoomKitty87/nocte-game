f522b5fa11b9257c758dc2640c2a09b145337784
Ansel Stanik <ansel.stanik@icloud.com>
2024-04-12 10:00:57 -0700

    working on extraction site stuff

--- Assets/_Scripts/Specific Objects/ExtractionGenerator.cs
+++ Assets/_Scripts/Specific Objects/ExtractionGenerator.cs
@@ -10,8 +10,20 @@ public class ExtractionGenerator : MonoBehaviour
 
   public string interactPrompt => "Activate Extractor";
 
+  [SerializeField] private Transform _lever;
+
   public void Interacted() {
     ExtractionSiteManager.Instance.ActivateExtractor(_extractorIndex);
+    StartCoroutine(Animation());
+  }
+
+  private IEnumerator Animation() {
+    float t = 0;
+    while (t < 1) {
+      _lever.localRotation = Quaternion.Euler(0, 0, Mathf.Lerp(50, -50, t));
+      t += Time.deltaTime;
+      yield return null;
+    }
   }
 
 }
