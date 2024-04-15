6c9184c1084b38bcff2b532a0d691749b6d0842d
Matthew Dinh <72724975+help-programming-is-hard@users.noreply.github.com>
2024-04-14 18:06:23 -0700

    temp fix

--- Assets/_Scripts/Interfaces/Interactable.cs
+++ Assets/_Scripts/Interfaces/Interactable.cs
@@ -7,9 +7,11 @@ public class Interactable : MonoBehaviour
   [SerializeField] private Collider _interactTrigger;
   public UnityEvent InteractedWith;
   
+  // Note: Errors in OnValidate are catastrophic
   private void OnValidate() {
     if (_interactTrigger == null) {
       Debug.LogError($"Interactable is lacking a trigger collider! Assign one in the inspector of {gameObject.name}");
+      return;
     }
     _interactTrigger.isTrigger = false;
   }
