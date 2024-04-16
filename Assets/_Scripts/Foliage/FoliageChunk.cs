861bf8ab92a7d406a6995fd15e8535447cbca9e0
Elliot <ejdalessa@gmail.com>
2024-04-16 14:45:00 -0700

    vfx event

--- Assets/_Scripts/Foliage/FoliageChunk.cs
+++ Assets/_Scripts/Foliage/FoliageChunk.cs
@@ -11,19 +11,17 @@ namespace Foliage
     private float _chunkSize;
     private Vector2 _chunkCenter;
 
-    public FoliageChunk() {
-      
-    }
+    public FoliageChunk() { }
     
     public FoliageChunk(IReadOnlyList<FoliageScriptable> scriptables, Vector2Int chunkPos, float chunkSize, Vector2 cameraPos) {
-      // var numberOfScriptables = scriptables.Count;
-      // _chunkSize = chunkSize;
-      // _chunkCenter = new Vector2(chunkPos.x * chunkSize, chunkPos.y * chunkSize);
-      // _renderers = new FoliageRenderer[numberOfScriptables];
-// 
-      // for (var i = 0; i < numberOfScriptables; i++) {
-      //   _renderers[i] = new FoliageRenderer(scriptables[i], chunkPos, chunkSize, cameraPos);
-      // }
+       var numberOfScriptables = scriptables.Count;
+       _chunkSize = chunkSize;
+       _chunkCenter = new Vector2(chunkPos.x * chunkSize, chunkPos.y * chunkSize);
+       _renderers = new FoliageRenderer[numberOfScriptables];
+ 
+       for (var i = 0; i < numberOfScriptables; i++) {
+         _renderers[i] = new FoliageRenderer(scriptables[i], chunkPos, chunkSize, cameraPos);
+       }
     }
 
 
