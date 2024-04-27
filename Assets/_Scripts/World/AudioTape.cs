e040ff2f81107abef5a00db8c2bf66392bc18431
DoomKitty87 <doomkitty887@gmail.com>
2024-04-27 16:15:30 -0700

    fixed tapes

--- Assets/_Scripts/World/AudioTape.cs
+++ Assets/_Scripts/World/AudioTape.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class AudioTape : MonoBehaviour
 {
-    
-    [SerializeField] private string name;
-    [SerializeField] private AudioClip audio;
-    [SerializeField] private Sprite icon;
+  
+  [SerializeField] private string name;
+  [SerializeField] private AudioClip audio;
+  [SerializeField] private Sprite icon;
 
-    public void Pickup() {
-        InventoryManager._instance.PickupAudioTape(name, audio, icon);
-    }
-    
+  [SerializeField] private GameObject deleteParent;
+
+  public void Pickup() {
+    InventoryManager._instance.PickupAudioTape(name, audio, icon);
+    Destroy(deleteParent);
+  }
+  
 }
