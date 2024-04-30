acef8e328a41e19f376b07b5ab27a1a3d3ccb0fb
SpikyLlama <62197004+SpikyLlama@users.noreply.github.com>
2024-04-30 09:59:23 -0700

    milksahek with them crushed up oreos

---
+++ Assets/_Scripts/Ecosystem/FoodManager.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodManager : MonoBehaviour
+{
+  private WorldGenerator _worldGenerator = WorldGenInfo._worldGenerator;
+  [SerializeField] private int _chunkDensity;
+
+  public void PlaceFoodPoints() {
+    // for each chunk pick like 5 points
+    // check each point to make sure they are not in the water
+    // if they are pick a new one
+    // after all points are valid get mesh height and instantiate at that height
+    
+  }
+}
