1d3e732d5fa0f1216faf5950fe1e9f54a9140df1
DoomKitty87 <doomkitty887@gmail.com>
2024-05-01 22:48:19 -0700

    error fix

--- Assets/_Scripts/Effects/SampleWorldToTexture.cs
+++ Assets/_Scripts/Effects/SampleWorldToTexture.cs
@@ -6,10 +6,10 @@ public class SampleWorldToTexture
   public static Texture2D SampleWorld(float scale, int resolution) {
     Texture2D tex = new Texture2D(resolution, resolution);
 
-    float[,] data = new float[resolution, resolution];
+    float[] data = new float[resolution * resolution];
     for (int x = 0; x < resolution; x++) {
       for (int y = 0; y < resolution; y++) {
-        data[x, y] = WorldGenerator.Instance.GetHeightValue(new Vector2(x, y) * scale);
+        data[x * resolution + y] = WorldGenInfo._worldGenerator.GetHeightValue(new Vector2(x, y) * scale);
       }
     }
 
