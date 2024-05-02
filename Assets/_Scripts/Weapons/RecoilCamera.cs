7ddedf3f773574b674a9896b50231c8d354faf38
Matthew Dinh <72724975+help-programming-is-hard@users.noreply.github.com>
2024-05-01 23:24:38 -0700

    aligned left hand ik

--- Assets/_Scripts/Weapons/RecoilCamera.cs
+++ Assets/_Scripts/Weapons/RecoilCamera.cs
@@ -18,22 +18,25 @@ public class RecoilCamera : MonoBehaviour
 {
     [SerializeField] private Transform _recoilTransform;
     [SerializeField] private RecoilProfile _currentProfile;
+    [SerializeField] private Vector3 _underlyingRotation;
     [SerializeField] private Vector3 _currentRotation;
     [SerializeField] private Vector3 _targetRotation;
 
     private void Start() {
-        _currentRotation = Vector3.zero;
+        _underlyingRotation = Vector3.zero;
         _targetRotation = Vector3.zero;
     }
 
     // Update is called once per frame
     void Update() {
         if (_currentProfile == null) {
-            _currentRotation = Vector3.zero;
+            return;
         }
-        _currentRotation = Vector3.Slerp(_currentRotation, _targetRotation, _currentProfile._recoilSnappiness * Time.deltaTime);
-        _recoilTransform.localRotation = Quaternion.Euler(_currentRotation);
+        _underlyingRotation = _recoilTransform.rotation.eulerAngles;
+        _currentRotation = Vector3.Lerp(_currentRotation, _targetRotation, _currentProfile._recoilSnappiness * Time.deltaTime);
+        _recoilTransform.rotation = Quaternion.Euler(_underlyingRotation + _currentRotation);
         _targetRotation = Vector3.Slerp(_targetRotation, Vector3.zero, _currentProfile._recoilRecoveryDegPerSecond * Time.deltaTime);
+        _underlyingRotation = _recoilTransform.rotation.eulerAngles - _currentRotation;
     }
 
     public void SetRecoilProfile(RecoilProfile profile) {
