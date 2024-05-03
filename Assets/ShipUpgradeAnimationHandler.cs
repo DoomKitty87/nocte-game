dd7343f03e7785d8b9c482e310df070aedb7eb57
Elliot <93895160+2Elliot@users.noreply.github.com>
2024-05-02 23:31:20 -0700

    started working on upgrade table

---
+++ Assets/ShipUpgradeAnimationHandler.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipUpgradeAnimationHandler : MonoBehaviour {
+    [SerializeField] private GameObject _upgradeScreenObject;
+    [SerializeField] private GameObject _homeScreenObject;
+    
+    public void ToggleUpgradeScreen(bool enable) {
+        _homeScreenObject.SetActive(!enable);
+        _upgradeScreenObject.SetActive(enable);
+    }
+    
+    
+}
