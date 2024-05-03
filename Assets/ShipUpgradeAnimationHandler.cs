a4ac27fd1f2ad945fc4e490a883e9d5384a50a2c
Elliot <93895160+2Elliot@users.noreply.github.com>
2024-05-02 23:37:08 -0700

    fixed bug with canvas

--- Assets/ShipUpgradeAnimationHandler.cs
+++ Assets/ShipUpgradeAnimationHandler.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class ShipUpgradeAnimationHandler : MonoBehaviour {
     [SerializeField] private GameObject _upgradeScreenObject;
+    [SerializeField] private GameObject _upgradeScreenCamera;
     [SerializeField] private GameObject _homeScreenObject;
     
     public void ToggleUpgradeScreen(bool enable) {
         _homeScreenObject.SetActive(!enable);
         _upgradeScreenObject.SetActive(enable);
+        _upgradeScreenCamera.SetActive(enable);
     }
     
     
