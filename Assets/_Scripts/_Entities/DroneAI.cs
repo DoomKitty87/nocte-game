09a3f06cba0c585fcd3dc6a8d75f50f74bd3a96c
DoomKitty87 <doomkitty887@gmail.com>
2024-05-03 22:29:30 -0700

    you can fight the drones now

--- Assets/_Scripts/_Entities/DroneAI.cs
+++ Assets/_Scripts/_Entities/DroneAI.cs
@@ -1,3 +1,4 @@
+using _Scripts._Entities.Creatures;
 using UnityEngine;
 
 public class DroneAI : MonoBehaviour
@@ -13,17 +14,36 @@ public class DroneAI : MonoBehaviour
   [SerializeField] private float _rotationSpeed;
 
   [SerializeField] private float _stopDistance;
+  [SerializeField] private float _attackDistance;
+
+  [SerializeField] private CreatureCombat _combat;
+
+  [SerializeField] private CreatureAttack _attack;
+
+  private float _attackTimer;
 
   private void Start() {
     _playerTarget = GameObject.FindGameObjectWithTag("Player").transform;
   }
 
   private void Update() {
-    Vector3 direction = _playerTarget.position - transform.position;
+    Vector3 direction = _playerTarget.position - transform.position + Vector3.up * 0.5f;
     Quaternion rotation = Quaternion.LookRotation(direction);
     transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * _rotationSpeed);
     float speed = _speedCurve.Evaluate((direction.magnitude - _stopDistance) / _maxSpeedDistance) * _maxSpeed;
     transform.position += transform.forward * Time.deltaTime * speed;
+
+    // Attempt to attack player
+    if (direction.magnitude < _attackDistance && _attackTimer > _attack._attackRepeatSeconds) {
+      _combat.Attack(_attack);
+      _attackTimer = 0;
+    }
+
+    _attackTimer += Time.deltaTime;
+  }
+
+  public void Neutralized() {
+    Destroy(gameObject);
   }
 
 }
