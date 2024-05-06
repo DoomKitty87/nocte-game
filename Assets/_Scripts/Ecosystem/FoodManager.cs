27c5a065750702b1401f5685d56a5aecec3ba140
SpikyLlama <62197004+SpikyLlama@users.noreply.github.com>
2024-05-06 10:01:29 -0700

    this is nearly worklying

--- Assets/_Scripts/Ecosystem/FoodManager.cs
+++ Assets/_Scripts/Ecosystem/FoodManager.cs
@@ -5,13 +5,15 @@ public class FoodManager : MonoBehaviour
 {
   private WorldGenerator _worldGenerator = WorldGenInfo._worldGenerator;
   [SerializeField] private int _chunkDensity;
+  public List<Vector3> _foodLocations;
 
   public void PlaceFoodPoints() {
-    
     // for each chunk pick like 5 points
     // check each point to make sure they are not in the water
     // if they are pick a new one
     // after all points are valid get mesh height and instantiate at that height
-    
+    foreach (var chunk in _chunkPool) {
+      x1 = chunk._chunkCenter + 
+    }
   }
 }
