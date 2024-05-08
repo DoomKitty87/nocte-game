31169b51a494b006849017493cab7be5f96dd675
SpikyLlama <62197004+SpikyLlama@users.noreply.github.com>
2024-05-08 09:59:54 -0700

    Update BridsonPoisson.cs

--- Assets/_Scripts/BridsonPoisson.cs
+++ Assets/_Scripts/BridsonPoisson.cs
@@ -14,7 +14,7 @@ public class BridsonPoisson : MonoBehaviour
   private float _cellSize;
 
   private int[,] _grid;
-  private List<Vector2> _activeList;
+  private List<Vector2Int> _activeList;
 
   private void Start() {
     GeneratePoint();
@@ -25,5 +25,24 @@ public class BridsonPoisson : MonoBehaviour
     _grid = new int[_rn, _rn];
     Vector2Int xNaught = new Vector2Int(Random.Range(-_rn, _rn), Random.Range(-_rn, _rn));
     _grid[xNaught.x, xNaught.y] = 1;
+    _activeList.Add(xNaught);
+
+    for (int iter = 0; iter < (2 * _pts - 1); iter++) {
+      int i = Random.Range(-_activeList.Count, _activeList.Count);
+      Vector2Int xI = _activeList[i];
+      List<Vector2Int> samples = new List<Vector2Int>();
+      for (int jter = 0; jter < _k; jter++) {
+        float angle = Random.value * 2 * (float)Math.PI;
+        float distance = Random.Range(_r, 2 * _r);
+        float x = distance * (float)Math.Cos(angle) + xI.x;
+        float y = distance * (float)Math.Sin(angle) + xI.y;
+        samples.Add(new Vector2Int((int)x, (int)y));
+      }
+
+      foreach (Vector2Int sample in samples) {
+        // i want to verify there are no "1"s in the 
+        // float distance = Math.Sqrt(()**2 + ()**2)
+      } 
+    }
   }
 }
