cee68c66daa0586ea51bb677d8dc32c87cc90bad
Matthew Dinh <72724975+help-programming-is-hard@users.noreply.github.com>
2024-05-15 22:50:13 -0700

    added first voice line
    kinda sucks but is okay

--- Assets/Dialogue/Dialogue.cs
+++ Assets/Dialogue/Dialogue.cs
@@ -6,23 +6,17 @@ using UnityEngine;
 [Serializable]
 public class DialogueTurn
 {
-
+	public Character _character;
 	[TextArea(3, 20)] public string _text;
 	public float _duration;
 	public AudioClip _audioClip;
 	public bool _durationIsAudioLength;
-  
 }
 
 [Serializable]
 [CreateAssetMenu(fileName = "New Dialogue", menuName = "Dialogue/Dialogue Object")]
 public class Dialogue : ScriptableObject
 {
-	
-	
-	public Character _character;
-	[Tooltip("Color of the character's name in the subtitle box.")]
-	[ColorUsage(false, false)] public Color _nameColor;
 	[Tooltip("List of text turns in the dialogue. Each turn will be displayed in the subtitle box.")]
 	public List<DialogueTurn> _textTurns;
 }
