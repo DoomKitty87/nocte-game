73db22885723c329b95a60d8c3db8b6ac4187fc1
DoomKitty87 <doomkitty887@gmail.com>
2024-05-18 01:55:06 -0700

    repositioned

--- Assets/_Scripts/UI/PlayerUI/PositionOverlayAsWorldSpace.cs
+++ Assets/_Scripts/UI/PlayerUI/PositionOverlayAsWorldSpace.cs
@@ -13,20 +13,21 @@ public class PositionOverlayAsWorldSpace : MonoBehaviour
 
   [SerializeField] private RectTransform _overlayConnection;
   [SerializeField] private Vector3 _connectionOffset;
+  [SerializeField] private Transform _connectionTarget;
   
 	private void Start() {
 		_overlay.position = Camera.main.WorldToScreenPoint(_target.position) + _offset;
-    _overlayConnection.position = Camera.main.WorldToScreenPoint(_target.position) + _connectionOffset;
+    _overlayConnection.position = Camera.main.WorldToScreenPoint(_connectionTarget.position) + _connectionOffset;
 	}
 
   public void ResetPosition() {
     _overlay.position = Camera.main.WorldToScreenPoint(_target.position) + _offset;
-    _overlayConnection.position = Camera.main.WorldToScreenPoint(_target.position) + _connectionOffset;
+    _overlayConnection.position = Camera.main.WorldToScreenPoint(_connectionTarget.position) + _connectionOffset;
   }
 
 	private void Update() {
 		Vector3 targetPosition = Camera.main.WorldToScreenPoint(_target.position) + _offset;
-    Vector3 connectionPosition = Camera.main.WorldToScreenPoint(_target.position) + _connectionOffset;
+    Vector3 connectionPosition = Camera.main.WorldToScreenPoint(_connectionTarget.position) + _connectionOffset;
     Vector3 newPosition = Vector3.Lerp(_overlay.position, targetPosition, _damping.x * Time.deltaTime);
     Vector3 newConnectionPosition = Vector3.Lerp(_overlayConnection.position, connectionPosition, _damping.x * Time.deltaTime);
     _overlay.position = newPosition;
